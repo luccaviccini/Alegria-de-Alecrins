f07dc63a5760d0ad051ec13793ad3a4d58c3955e
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-06-25 17:13:22 -0300

    feat: basic folder structure

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,11 @@
 import 'express-async-errors'
 import express, {json} from 'express'
+import cors from 'cors'
 
+
+const app = express()
+app.use(cors())
+app.use(json())
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server listening on port ${port}`))
